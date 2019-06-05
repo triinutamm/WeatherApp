64fae73c8dcd027f5fd4cca6c50bd9089589ccf0
triinutamm <triinu.tamm@tptlive.ee>
2019-06-05 12:14:17 +0300

    eeeeeeeeeee jah

--- WeatherApp1.1/MainActivity.cs
+++ WeatherApp1.1/MainActivity.cs
@@ -13,11 +13,8 @@ namespace WeatherApp1._1
 
     public class MainActivity : AppCompatActivity
     {   
-        //static string city = "Tallinn";
-        //static EditText cityname_ = (EditText)FindViewById(Resource.Id.cityname);
-        //static string city = cityname_.getText().toString();
         string queryurl;
-        static string result = "result error";
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -27,35 +24,23 @@ namespace WeatherApp1._1
 
             var Button = FindViewById<Button>(Resource.Id.button1);
             Button.Click += Button_Click;
-
-            //TextView Ilmainfo = FindViewById<TextView>(Resource.Id.ilmainfo);
-            //Ilmainfo.Text = queryurl;
-
         }
         private async void Button_Click(object sender, System.EventArgs e)
         {
             var cityname_ = FindViewById<EditText>(Resource.Id.cityname);
             string city = cityname_.Text.ToString();
             queryurl = "https://openweathermap.org/data/2.5/weather?q=" + city + "&appid=b6907d289e10d714a6e88b30761fae22";
+
             HttpClient client = new HttpClient();
             HttpResponseMessage response = await client.GetAsync(queryurl);
             HttpContent content = response.Content;
-            string result = await content.ReadAsStringAsync();
-            TextView Ilmainfo = FindViewById<TextView>(Resource.Id.ilmainfo);
-            Ilmainfo.Text = result;
-            //return result;
+            dynamic result = await content.ReadAsStringAsync();
+        
+
+
+            TextView Wind = FindViewById<TextView>(Resource.Id.wind);
+            Wind.Text = result;
 
         }
-        //public static async Task<string> Data(string apiurl="https://openweathermap.org/data/2.5/weather?q=Tallinn,est&appid=b6907d289e10d714a6e88b30761fae22")
-        //{
-        //    HttpClient client = new HttpClient();
-        //    HttpResponseMessage response = await client.GetAsync(apiurl);
-        //    HttpContent content = response.Content;
-        //    string result = await content.ReadAsStringAsync();
-        //    return result;
-           
-
-        //}
-
     }
 }
